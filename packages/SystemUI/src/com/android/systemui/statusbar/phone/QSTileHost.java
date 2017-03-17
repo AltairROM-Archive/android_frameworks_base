b8fc93a9f3d65e5936a2daf838b85a87b0746a96
Ali B <abittin@gmail.com>
2017-03-17 13:27:27 +0300

    QS: Add Substratum tile
    PS2: add Showcase activity to longpress
    PS3: remove from tiles if not available
    
    Change-Id: Ibc3189f1741282961a71ea7695311437d47a0303
    Signed-off-by: Ali B <abittin@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/values/aicp_strings.xml
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -63,6 +63,7 @@ import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.NavigationBarTile;
 import com.android.systemui.qs.tiles.PulseTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.SubstratumTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -466,6 +467,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("navigation_bar")) return new NavigationBarTile(this);
         else if (tileSpec.equals("pulse")) return new PulseTile(this);
         else if (tileSpec.equals("magiskmanager")) return new MagiskManagerTile(this);
+        else if (tileSpec.equals("substratum")) return new SubstratumTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
