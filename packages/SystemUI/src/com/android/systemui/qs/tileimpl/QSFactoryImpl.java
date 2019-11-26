04c8a0f6450adf04a8cb493ca47cc891b6d4199d
Christian Oder <myself5@carbonrom.org>
2019-11-26 23:35:01 +0100

    SystemUI: Introduce DataSwitchTile
    * Based on OnePlus' OxygenOS tile, reworked to work with AOSP toggling without requirements on proprietary telephony-ext features
    
    @neobuddy89: Adapted to Android 12 and squashed below -
    
        From: DennySPB <dennyspb@gmail.com>
        Date: Thu, 5 Dec 2019 12:40:17 +0300
        Subject: DataSwitchTile: collapse notification panel onClick
    
        Change-Id: I37a064c910ea05493b41bdd0123ca3a6aca1f25d
        Signed-off-by: DennySPB <dennyspb@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: micky387 <mickaelsaibi@free.fr>
        Date: Wed, 11 Mar 2020 13:36:51 +0100
        Subject: DataSwitchTile: dont show toast on click
    
        and add drawable for No SIM
    
        Change-Id: Ib9d26630879420216e67de1a1e6fc82c5c2ace5e
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: DennySPb <dennyspb@gmail.com>
        Date: Tue, 17 Nov 2020 11:39:43 +0300
        Subject: SystemUI: Show carrier name of opposite slot in DataSwitch tile label
    
        make it more user friendly
    
        Change-Id: I53094db21fe21b1a4bd9ee76fd28661622eb5e26
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Wed, 14 Jul 2021 01:43:13 +0900
        Subject: DataSwitchTile: Improve the code
    
        * Get rid of deprecated AsyncTask
        * Removed unused imports
        * Finalize global vars
        * Some other formatting and minor stuff
    
        Change-Id: Id3e39c98dac6200301d15bf752f866f47875633d
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: Ie2e280c07f24f9da6b4ee218b72501a2713ce429
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,6 +39,7 @@ import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
+import com.android.systemui.qs.tiles.DataSwitchTile;
 import com.android.systemui.qs.tiles.DeviceControlsTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
@@ -117,6 +118,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<SoundTile> mSoundTileProvider;
     private final Provider<SmartPixelsTile> mSmartPixelsTileProvider;
     private final Provider<AudioModeTile> mAudioModeTileProvider;
+    private final Provider<DataSwitchTile> mDataSwitchTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -164,7 +166,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<VpnTile> vpnTileProvider,
             Provider<SoundTile> soundTileProvider,
             Provider<SmartPixelsTile> smartPixelsTileProvider,
-            Provider<AudioModeTile> audioModeTileProvider) {
+            Provider<AudioModeTile> audioModeTileProvider,
+            Provider<DataSwitchTile> dataSwitchTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -208,6 +211,7 @@ public class QSFactoryImpl implements QSFactory {
         mSoundTileProvider = soundTileProvider;
         mSmartPixelsTileProvider = smartPixelsTileProvider;
         mAudioModeTileProvider = audioModeTileProvider;
+        mDataSwitchTileProvider = dataSwitchTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -301,6 +305,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mSmartPixelsTileProvider.get();
             case "audio_mode":
                 return mAudioModeTileProvider.get();
+            case "dataswitch":
+                return mDataSwitchTileProvider.get();
         }
 
         // Custom tiles
