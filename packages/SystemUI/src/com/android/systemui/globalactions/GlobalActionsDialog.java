650f66dd5b3a591ab67a381648adccbcb931f7a6
Pranav Vashi <neobuddy89@gmail.com>
2021-05-26 18:32:57 +0530

    PowerMenu: Remove redundant immediate dismiss
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -2645,7 +2645,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             mPowerOptionsDialog.show();
         }
 
-        public void showRestartOptionsMenu() {
+        public void showRestartOptionsMenu() { 
             mRestartOptionsDialog = GlobalActionsPowerDialog.create(
                     mContext, mRestartOptionsAdapter);
             mRestartOptionsDialog.show();
@@ -2834,9 +2834,9 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
 
                 // close first, as popup windows will not fade during the animation
                 dismissOverflow(false);
-                dismissPowerOptions(false);
-                dismissRestartOptions(false);
-                dismissUsers(false);
+                dismissPowerOptions();
+                dismissRestartOptions();
+                dismissUsers();
                 if (mControlsUiController != null) mControlsUiController.closeDialogs(false);
             });
         }
@@ -2861,9 +2861,9 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             resetOrientation();
             dismissWallet();
             dismissOverflow(true);
-            dismissPowerOptions(true);
-            dismissRestartOptions(true);
-            dismissUsers(true);
+            dismissPowerOptions();
+            dismissRestartOptions();
+            dismissUsers();
             if (mControlsUiController != null) mControlsUiController.hide();
             mNotificationShadeWindowController.setRequestTopUi(false, TAG);
             mDepthController.updateGlobalDialogVisibility(0, null /* view */);
@@ -2890,33 +2890,21 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             }
         }
 
-        private void dismissPowerOptions(boolean immediate) {
+        private void dismissPowerOptions() {
             if (mPowerOptionsDialog != null) {
-                if (immediate) {
-                    mPowerOptionsDialog.dismiss();
-                } else {
-                    mPowerOptionsDialog.dismiss();
-                }
+                mPowerOptionsDialog.dismiss();
             }
         }
 
-        private void dismissRestartOptions(boolean immediate) {
+        private void dismissRestartOptions() {
             if (mRestartOptionsDialog != null) {
-                if (immediate) {
-                    mRestartOptionsDialog.dismiss();
-                } else {
-                    mRestartOptionsDialog.dismiss();
-                }
+                mRestartOptionsDialog.dismiss();
             }
         }
 
-        private void dismissUsers(boolean immediate) {
+        private void dismissUsers() {
             if (mUsersDialog != null) {
-                if (immediate) {
-                    mUsersDialog.dismiss();
-                } else {
-                    mUsersDialog.dismiss();
-                }
+                mUsersDialog.dismiss();
             }
         }
 
@@ -2963,9 +2951,9 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             // ensure dropdown menus are dismissed before re-initializing the dialog
             dismissWallet();
             dismissOverflow(true);
-            dismissPowerOptions(true);
-            dismissRestartOptions(true);
-            dismissUsers(true);
+            dismissPowerOptions();
+            dismissRestartOptions();
+            dismissUsers();
             if (mControlsUiController != null) {
                 mControlsUiController.hide();
             }
