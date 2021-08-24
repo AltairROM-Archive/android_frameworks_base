748a68de5686be583bc70196a4f6528b84b91f86
Martin J. Pollard <martin0566@gmail.com>
2021-08-24 15:52:10 -0400

    Navbar: Introduce customizable navbar and hardware buttons [1/2]
    - Customize long press and double tap behavior for home, back,
      recents, menu, and assist buttons, and short press behavior for
      recents, menu, and assist.
    - Added actions to toggle flashlight, clear notifications, display
      volume panel, toggle notification panel, display power menu, take
      screenshot, toggle QS panel, set ringer mode, and kill foreground
      app.
    - Added action to run custom app on long press or double tap of home,
      back, and recents buttons.
    - Added new navbar icons to indicate short press actions assigned to
      buttons (only on recents for now, as short press actions on home
      and back buttons cannot be changed).
    
    Note that most of this code was taken from Havoc-OS and adapted to
    LineageOS, with only a few changes and additions by me. As such, all
    due credit goes to the Havoc-OS team for these changes, for which I
    send my sincere thanks!
    
    The Havoc-OS commits that were the basis for these changes are:
    
    Havoc-OS/android_frameworks_base@37e7be8
    Havoc-OS/android_frameworks_base@fce3e93
    Havoc-OS/android_frameworks_base@2d3ab2d
    Havoc-OS/android_frameworks_base@03e2099
    Havoc-OS/android_frameworks_base@57d07c3
    Havoc-OS/android_frameworks_base@b09ba95
    Havoc-OS/android_frameworks_base@56e0a14
    Havoc-OS/android_frameworks_base@be3ed4a
    Havoc-OS/android_frameworks_base@04ba59c
    Havoc-OS/android_frameworks_base@4a7bf58
    Havoc-OS/android_frameworks_base@970aa2a
    Havoc-OS/android_frameworks_base@7559af4
    Havoc-OS/android_frameworks_base@457be70
    Havoc-OS/android_frameworks_base@f32c845
    Havoc-OS/android_frameworks_base@b3ea152
    Havoc-OS/android_frameworks_base@05d91d3
    Havoc-OS/android_frameworks_base@0cb528d
    
    These commits were originally for Android 10, and have been updated
    for Android 11.

--- services/core/java/com/android/server/policy/WindowManagerPolicy.java
+++ services/core/java/com/android/server/policy/WindowManagerPolicy.java
@@ -69,6 +69,7 @@ import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.app.WindowConfiguration;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.CompatibilityInfo;
 import android.content.res.Configuration;
 import android.graphics.Rect;
@@ -1467,4 +1468,9 @@ public interface WindowManagerPolicy extends WindowManagerPolicyConstants {
      * @return whether the value was changed.
      */
     boolean setAodShowing(boolean aodShowing);
+
+    /**
+     * Send some ActionHandler commands to WindowManager.
+     */
+    public void sendCustomAction(Intent intent);
 }
