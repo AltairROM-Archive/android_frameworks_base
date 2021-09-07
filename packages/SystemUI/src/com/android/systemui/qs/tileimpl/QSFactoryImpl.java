abc7c0a4feafa7034c5e9fa6f702219948f7452b
Martin J. Pollard <martin0566@gmail.com>
2021-09-06 20:27:15 -0400

    SystemUI: Add tile to toggle mono audio
    Toggles the Accessibility setting to combine the left and right audio
    channels into a single mono channel. Handy for using a single earbud or
    a Bluetooth device with only one speaker, so that one audio channel
    doesn't "disappear."

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LiveDisplayTile;
+import com.android.systemui.qs.tiles.MonoAudioTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.PowerMenuTile;
@@ -113,6 +114,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<SmartPixelsTile> mSmartPixelsTileProvider;
     private final Provider<RebootTile> mRebootTileProvider;
     private final Provider<SoundTile> mSoundTileProvider;
+    private final Provider<MonoAudioTile> mMonoAudioTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
 
@@ -154,7 +156,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<AntiFlickerTile> antiFlickerTileProvider,
             Provider<SmartPixelsTile> smartPixelsTileProvider,
             Provider<RebootTile> rebootTileProvider,
-            Provider<SoundTile> soundTileProvider) {
+            Provider<SoundTile> soundTileProvider,
+            Provider<MonoAudioTile> monoAudioTileProvider) {
         mQsHostLazy = qsHostLazy;
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
@@ -193,6 +196,7 @@ public class QSFactoryImpl implements QSFactory {
         mSmartPixelsTileProvider = smartPixelsTileProvider;
         mRebootTileProvider = rebootTileProvider;
         mSoundTileProvider = soundTileProvider;
+        mMonoAudioTileProvider = monoAudioTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -279,6 +283,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mRebootTileProvider.get();
             case "sound":
                 return mSoundTileProvider.get();
+            case "mono_audio":
+                return mMonoAudioTileProvider.get();
         }
 
         // Custom tiles
