1bc30a4e02ca4fe41edd1a242de3e3c9b94f2363
Martin J. Pollard <martin0566@gmail.com>
2022-05-10 15:51:54 -0400

    base: Show icon of package associated with Toast [1/2]
    For all those times you have some random app or background service that posts
    a Toast and you have no idea who's posting it. This adds an icon badge to the
    top left corner of the Toast to show the app's icon the Toast belongs to.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5259,6 +5259,12 @@ public final class Settings {
          */
         public static final String DATA_DISABLED_ICON = "data_disabled_icon";
 
+        /**
+         * Toast icon
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
         /**********************************/
         /* Altair ROM System Settings End */
         /**********************************/
